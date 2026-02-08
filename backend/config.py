9f55dd3a6ca8e50cf6316928e23db280c955aa79
DURBA KUSHARI <durbakushari1997@gmail.com>
2026-02-08 16:27:40 +0530

    Delete DB_CONFIG from config.py
    Remove database configuration from config.py

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,1 @@
-# config.py
-DB_CONFIG = {
-    "host": "127.0.0.1",      # localhost for Docker
-    "port": 5432,
-    "database": "ehospital",
-    "user": "admin",
-    "password": "yourpassword"  # replace with your Postgres password
-}
+
